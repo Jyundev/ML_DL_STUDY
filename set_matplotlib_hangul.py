b597e3f08c7635980a12f36d3609803f4495a48d
JyunDev <jyunxxxxx@gmail.com>
2023-05-15 17:14:25 +0900

    initial sett

---
+++ set_matplotlib_hangul.py
@@ -0,0 +1,26 @@
+
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import seaborn as sns
+import platform
+
+# %matplotlib inline
+get_ipython().run_line_magic("matplotlib","inline")
+
+path='C:/Windows/Fonts/malgun.ttf'
+plt.rcParams['axes.unicode_minus'] = False
+
+if platform.system()=="Darwin": #mac
+    rc("font",family='Arial Unicodes MS')
+    sns.set(font='Arial Unicodes MS', 
+        rc={"axes.unicode_minus":False}),
+    print('MAC Hangul OK')
+elif platform.system()=="Windows":  #window
+    font_name=font_manager.FontProperties(fname=path).get_name()
+    rc("font",family=font_name)
+    sns.set(font=font_name, 
+    rc={"axes.unicode_minus":False})
+    print('WIndow Hangul OK')
+    
+else:
+    print('Unknown System')
